766cdb3b1490b9665df150c6802d43191bf37e9f
Saam <saamjwesley@gmail.com>
2022-03-29 22:15:49 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,34 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)  # xpos, ypos, width, height
+        self.setWindowTitle("Attendance Test")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Text test.")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)  # (function_name)
+
+    def clicked(self):
+        self.label.setText("Button Clicked")
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+
+    win.show()  # displays the output screen
+    sys.exit(app.exec_())  # for clean exit.
+
+
+window()
